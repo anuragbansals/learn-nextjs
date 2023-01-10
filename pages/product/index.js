3aea851c1c72e22340faca5e00acf9989258922b
Anurag Bansal <anuragbansaldps@gmail.com>
2023-01-10 21:49:44 +0530

    404 page, links in navigation

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,23 @@
-function ProductList() {
+import Link from "next/link";
+
+function ProductList({ productId = 11 }) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">Home</Link>
+      <Link href="/product/1">
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/product/2">
+        <h2>Product 2</h2>
+      </Link>
+      <Link href="/product/3" replace>
+        <h2>Product 3</h2>
+      </Link>
+      <Link href={`/product/${productId}`}>
+        <h2>Product {productId}</h2>
+      </Link>
+      {/* <h2>Product 2</h2>
+      <h2>Product 3</h2> */}
     </>
   );
 }
